e8886e69a2a46a4d68881f89421187db0441f79f
DovnarAlexander <dovnaralexander@users.noreply.github.com>
2025-06-18 15:55:32 +0300

    fix(extra): moved FORCE_UPDATE_CHART to higher level

--- hooks/helm/checks-dependencies.sh
+++ hooks/helm/checks-dependencies.sh
@@ -4,18 +4,18 @@ set -e
 
 CHART_ARG=( "$@" )
 
-if [ -z "${CHART_ARG[*]}" ]; then
-    echo "🔍 Auto-detecting changed Helm charts from git..."
-    CHART_ARG=$(git diff --cached --name-only | grep -E '(^|/)Chart\.yaml$' | xargs -n1 dirname | paste -sd, -)
+if [ -n "${FORCE_UPDATE_CHART}" ]; then
+    CHART_ARG=("${FORCE_UPDATE_CHART}")
+else
     if [ -z "${CHART_ARG[*]}" ]; then
-        if [ -n "${FORCE_UPDATE_CHART}" ]; then
-            CHART_ARG="${FORCE_UPDATE_CHART}"
-        else
+        echo "🔍 Auto-detecting changed Helm charts from git..."
+        CHART_ARG=$(git diff --cached --name-only | grep -E '(^|/)Chart\.yaml$' | xargs -n1 dirname | paste -sd, -)
+        if [ -z "${CHART_ARG[*]}" ]; then
             echo "✅ No changed charts detected, skipping"
             exit 0
         fi
+        echo "📦 Charts to update: $CHART_ARG"
     fi
-    echo "📦 Charts to update: $CHART_ARG"
 fi
 
 # Temporary files
